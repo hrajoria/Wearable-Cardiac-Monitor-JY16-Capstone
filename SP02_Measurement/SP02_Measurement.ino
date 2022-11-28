8bf8665d799464a8de129797585b2abb60cd2cf9
tawfi11 <nusair11@gmail.com>
2022-11-28 05:26:45 -0800

    Added some plots

---
+++ SP02_Measurement/SP02_Measurement.ino
@@ -0,0 +1,44 @@
+#include <Wire.h>
+#include "MAX30105.h"
+
+#include "heartRate.h"
+#include "spo2_algorithm.h"
+
+MAX30105 particleSensor;
+
+const int32_t bufferLength = 100; //data length
+uint32_t redBuffer[bufferLength]; //Array of red values
+uint32_t irBuffer[bufferLength]; //array of ir values
+int32_t spo2; //SPO2 value
+int8_t validSPO2; //indicator to show if the SPO2 calculation is valid
+int32_t heartRate; //heart rate value
+int8_t validHeartRate; //indicator to show if the heart rate calculation is valid
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Initializing...");
+
+  // Initialize sensor
+  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
+  {
+    Serial.println("MAX30105 was not found. Please check wiring/power. ");
+    while (1);
+  }
+  Serial.println("Place your index finger on the sensor with steady pressure.");
+
+  particleSensor.setup(); //Configure sensor with default settings
+  particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
+  particleSensor.setPulseAmplitudeGreen(0); //Turn off Green LED
+}
+
+void loop() {
+  for(int i = 0; i < bufferLength; i++){
+    while (particleSensor.available() == false) //do we have new data?
+      particleSensor.check(); //Check the sensor for new data
+    redBuffer[i] = particleSensor.getRed();
+    irBuffer[i] = particleSensor.getIR();
+    particleSensor.nextSample(); //We're finished with this sample so move to next sample
+  }
+  maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
+  Serial.printf("HR: %d (%d)\tSpO2: %d (%d)\n", heartRate, validHeartRate, spo2, validSPO2);
+}
